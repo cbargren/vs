5fea466048f16fb43241a00c4b74df33a081cc90
Christopher Bargren <cbargren@gmail.com>
2017-01-20 11:32:24 -0700

    setScore implementation for LMS

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 
 import { ClipsInit, StatusReported } from './state/actions';
 import store from './state/store';
-import { getAllClipsAreCorrect } from './state/selectors';
+import { getAllClipsAreCorrect, getAllClipsOrdered, getNumberOfClips, getNumberOfCorrectlyOrderedClips } from './state/selectors';
 import './App.css';
 import Container from './components/Container';
 import LMS from './LMSMock';
@@ -18,11 +18,18 @@ class App extends Component {
 
   updateState() {
     const state = store.getState();
+    const numberOfClips = getNumberOfClips(state.clipsById);
+    const numberOfCorrectlyOrderedClips = getNumberOfCorrectlyOrderedClips(state.clipsById);
     const allClipsAreCorrect = getAllClipsAreCorrect(state.clipsById);
+    const allClipsOrdered = getAllClipsOrdered(state.clipsById);
 
     if (allClipsAreCorrect && !state.statusReported) {
       LMS.setStatus('complete');
+      LMS.setScore(100);
       store.dispatch(StatusReported(true));
+    }
+    else if (allClipsOrdered) {
+      LMS.setScore(+(100 * numberOfCorrectlyOrderedClips / numberOfClips).toFixed(2));
     }
 
     this.setState(state);
--- src/LMSMock.js
+++ src/LMSMock.js
@@ -1,5 +1,9 @@
 export default {
   setStatus(value) {
     console.log(`LMS status set to: ${value}`);
+  },
+
+  setScore(value) {
+    console.log(`LMS score set to ${value}`);
   }
 }
